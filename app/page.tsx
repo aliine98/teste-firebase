c55e0a83e08600963f90fc01feba7f1004058ef1
Aline <90913523+aliine98@users.noreply.github.com>
2024-03-01 22:34:39 +0000

    fix: adiciona hostname ao next.config e tipagem no imageList state

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,68 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { storage } from './firebase';
-import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
-import * as React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { useEffect, useState, useReducer } from 'react';
-import Alunos from './alunos';
+import Image from "next/image";
+import { storage } from "./firebase";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import * as React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useEffect, useState, useReducer } from "react";
+import Alunos from "./alunos";
 
 export default function Home() {
-    const [image, setImage] = useState(null);
-    const [imageList, setImageList] = useState([]);
-    const storageRef = ref(storage, 'galeria');
-    useEffect(() => {
-        listAll(storageRef).then((res) => {
-            res.items.forEach((item) => {
-                getDownloadURL(item).then((url) => {
-                    setImageList((prev) => [...prev, url]);
-                });
-            });
+  const [image, setImage] = useState(null);
+  const [imageList, setImageList] = useState<string[]>([]);
+  const storageRef = ref(storage, "galeria");
+  useEffect(() => {
+    listAll(storageRef).then((res) => {
+      res.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
         });
-    }, []);
+      });
+    });
+  }, []);
 
-    const uploadImage = () => {
-        if (image === null) return;
-        const imageRef = ref(storage, `galeria/${image!.name}`);
-        uploadBytes(imageRef, image).then((snapshot) => {
-            console.log(snapshot.metadata.name);
-            getDownloadURL(snapshot.ref).then((url) => {
-                setImageList((prev) => [...prev, url]);
-            });
-        });
-    };
+  const uploadImage = () => {
+    if (image === null) return;
+    const imageRef = ref(storage, `galeria/${image!.name}`);
+    uploadBytes(imageRef, image).then((snapshot) => {
+      console.log(snapshot.metadata.name);
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, url]);
+      });
+    });
+  };
 
-    return (
-        <>
-            <Form.Control
-                type="file"
-                accept="image/*"
-                onChange={(e) => setImage(e.target.files[0])}
-            />
-            <button onClick={uploadImage}>Adicionar imagem</button>
-            <ul>
-                {imageList.map((url, index) => (
-                    <img src={url} key={index} width={200} />
-                ))}
-            </ul>
-            <h1>Alunos</h1>
-            <p>Adicionar alunos</p>
-            <form>
-                <label>
-                    Nome: <input type="text" />
-                </label>
-                <label>
-                    Faixa: <input type="text" />
-                </label>
-                <label>
-                    Foto: <input type="file" accept="image/*" />
-                </label>
-                <input type="submit" />
-            </form>
+  return (
+    <>
+      <Form.Control
+        type="file"
+        accept="image/*"
+        onChange={(e) => setImage(e.target.files[0])}
+      />
+      <button onClick={uploadImage}>Adicionar imagem</button>
+      <ul>
+        {imageList.map((url, index) => (
+          <Image src={url} key={index} width={200} height={200} alt="" />
+        ))}
+      </ul>
+      <h1>Alunos</h1>
+      <p>Adicionar alunos</p>
+      <form>
+        <label>
+          Nome: <input type="text" />
+        </label>
+        <label>
+          Faixa: <input type="text" />
+        </label>
+        <label>
+          Foto: <input type="file" accept="image/*" />
+        </label>
+        <input type="submit" />
+      </form>
 
-            <Alunos />
-        </>
-    );
+      <Alunos />
+    </>
+  );
 }
