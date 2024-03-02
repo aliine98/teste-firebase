87ac69ca978c0ccec9689e92cd3010f2dbaef2be
Aline <90913523+aliine98@users.noreply.github.com>
2024-03-02 00:49:52 +0000

    feat: opçao de deletar imagens

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,36 @@
 
 import Image from "next/image";
 import { storage } from "./firebase";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
 import * as React from "react";
 import Form from "react-bootstrap/Form";
 import { useEffect, useState, useReducer } from "react";
 import Alunos from "./alunos";
+
+interface IAluno {
+  name: string;
+  belt: string;
+  imageName: string;
+}
+
+function reducer(state: IAluno, action: { type: string; payload: string }) {
+  switch (action.type) {
+    case "addName":
+      return { ...state, name: action.payload };
+    case "addBelt":
+      return { ...state, belt: action.payload };
+    case "addImage":
+      return { ...state, imageName: action.payload };
+    default:
+      return state;
+  }
+}
 
 export default function Home() {
   const [image, setImage] = useState<File | null>(null);
@@ -26,10 +51,16 @@ export default function Home() {
     if (image === null) return;
     const imageRef = ref(storage, `galeria/${image?.name}`);
     uploadBytes(imageRef, image).then((snapshot) => {
-      console.log(snapshot.metadata.name);
       getDownloadURL(snapshot.ref).then((url: string) => {
         setImageList((prev: string[]) => [...prev, url]);
       });
+    });
+  };
+
+  const deleteImage = (url: string) => {
+    const imageRef = ref(storage, url);
+    deleteObject(imageRef).then(() => {
+      setImageList(imageList.filter((image: string) => image !== url));
     });
   };
 
@@ -43,7 +74,10 @@ export default function Home() {
       <button onClick={uploadImage}>Adicionar imagem</button>
       <ul>
         {imageList.map((url: string, index: number) => (
-          <Image src={url} key={index} width={200} height={200} alt="" />
+          <li key={index}>
+            <Image src={url} key={index} width={200} height={200} alt="" />
+            <button onClick={() => deleteImage(url)}>Remover</button>
+          </li>
         ))}
       </ul>
       <h1>Alunos</h1>
