120b113e730c2d92dcd7f66de614b47fb0d1b0aa
Aline <90913523+aliine98@users.noreply.github.com>
2024-03-08 03:02:32 +0000

    teste mui e paginação

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { Inter } from "next/font/google";
 import { storage, db } from "./firebase";
 import {
   ref,
@@ -8,15 +9,40 @@ import {
   listAll,
   getDownloadURL,
   deleteObject,
+  list,
+  ListResult,
 } from "firebase/storage";
 import * as React from "react";
-import Form from "react-bootstrap/Form";
+import Button from "@mui/material/Button";
 import { useEffect, useState, useReducer } from "react";
 import Alunos from "./alunos";
 import { collection, addDoc } from "firebase/firestore";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+import Link from "@mui/material/Link";
+import {
+  FormControl,
+  FormGroup,
+  IconButton,
+  Input,
+  TextField,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+import { CloudUpload } from "@mui/icons-material";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#B53F41",
+    },
+  },
+  typography: {
+    fontFamily: inter.style.fontFamily,
+  },
+});
 
 interface IAluno {
   name: string;
@@ -30,8 +56,20 @@ export default function Home() {
   const [image, setImage] = useState<File | null>(null);
   const [imageList, setImageList] = useState<string[]>([]);
   const storageRef = ref(storage, "galeria");
+  const firstPage = list(storageRef, { maxResults: 2 });
+  const [nextPage, setNextPage] = useState<ListResult>();
+  // useEffect(() => {
+  //   listAll(storageRef).then((res) => {
+  //     res.items.forEach((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         setImageList((prev) => [...prev, url]);
+  //       });
+  //     });
+  //   });
+  // }, []);
+
   useEffect(() => {
-    listAll(storageRef).then((res) => {
+    firstPage.then((res) => {
       res.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
           setImageList((prev) => [...prev, url]);
@@ -39,6 +77,37 @@ export default function Home() {
       });
     });
   }, []);
+
+  const loadNextGaleryPage = async () => {
+    if (nextPage) {
+      const page = await list(storageRef, {
+        maxResults: 2,
+        pageToken: nextPage!.nextPageToken,
+      });
+      if (page.nextPageToken) {
+        setImageList([]);
+        page.items.forEach((item) => {
+          getDownloadURL(item).then((url) => {
+            setImageList((prev) => [...prev, url]);
+          });
+        });
+        setNextPage(page);
+      }
+    }
+    const page = await list(storageRef, {
+      maxResults: 2,
+      pageToken: (await firstPage).nextPageToken,
+    });
+    if (page.nextPageToken) {
+      setImageList([]);
+      page.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        });
+      });
+      setNextPage(page);
+    }
+  };
 
   const uploadImage = () => {
     if (image === null) return;
@@ -93,76 +162,112 @@ export default function Home() {
 
   return (
     <>
-      <Form onSubmit={(e) => e.preventDefault()}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email:</Form.Label>
-          <Form.Control
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Form.Label>Senha:</Form.Label>
-          <Form.Control
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <button onClick={() => signIn(email, password)}>Entrar</button>
-        <Link href="/forgot-password">Esqueci minha senha</Link>
-      </Form>
-      {user && (
-        <div>
-          <Form.Control
-            type="file"
-            accept="image/*"
-            onChange={(e) => setImage((e.target as HTMLInputElement).files![0])}
-          />
-          <button onClick={uploadImage}>Adicionar imagem</button>
-        </div>
-      )}
-      <ul>
-        {imageList.map((url: string, index: number) => (
-          <li key={index}>
-            <Image src={url} key={index} width={200} height={200} alt="" />
-            {user && <button onClick={() => deleteImage(url)}>Remover</button>}
-          </li>
-        ))}
-      </ul>
-      <h1>Alunos</h1>
-      {user && (
+      <ThemeProvider theme={theme}>
         <form onSubmit={(e) => e.preventDefault()}>
-          <label>
-            Nome:{" "}
-            <input
-              type="text"
-              onChange={(e) => setAluno({ ...aluno, name: e.target.value })}
+          <FormControl>
+            <TextField
+              type="email"
+              label="Email"
+              size="small"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              sx={{ mb: 2 }}
             />
-          </label>
-          <label>
-            Faixa:{" "}
-            <input
-              type="text"
-              onChange={(e) => setAluno({ ...aluno, belt: e.target.value })}
+            <TextField
+              type="password"
+              label="Senha"
+              size="small"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              sx={{ mb: 2 }}
             />
-          </label>
-          <label>
-            Foto:{" "}
-            <input
-              type="file"
-              accept="image/*"
-              onChange={(e) => {
-                setAluno({
-                  ...aluno,
-                  photo: (e.target as HTMLInputElement).files![0],
-                  photoName: (e.target as HTMLInputElement).files![0].name,
-                });
-              }}
-            />
-          </label>
-          <button onClick={addAluno}>Adicionar</button>
+            <Button
+              variant="contained"
+              // sx={{
+              //   backgroundColor: "#3234a8",
+              //   mb: 2,
+              //   "&:hover": { backgroundColor: "#393ce3" },
+              // }}
+              size="small"
+              onClick={() => signIn(email, password)}
+            >
+              Entrar
+            </Button>
+            <Link href="/forgot-password" variant="body2" underline="always">
+              Esqueci minha senha
+            </Link>
+          </FormControl>
         </form>
-      )}
+        {user && (
+          <div>
+            <Button startIcon={<CloudUpload />} sx={{ position: "relative" }}>
+              {image ? image?.name.slice(0, 25) + "..." : "Selecionar Foto"}
+              <input
+                type="file"
+                accept="image/*"
+                style={{ position: "absolute", opacity: 0, cursor: "pointer" }}
+                onChange={(e) =>
+                  setImage((e.target as HTMLInputElement).files![0])
+                }
+              />
+            </Button>
+            <Button variant="contained" onClick={uploadImage}>
+              Adicionar
+            </Button>
+            <Button variant="contained" color="error" onClick={uploadImage}>
+              Adicionar
+            </Button>
+          </div>
+        )}
+        <ul>
+          {imageList.map((url: string, index: number) => (
+            <li key={index}>
+              <Image src={url} key={index} width={200} height={200} alt="" />
+              {user && (
+                <button onClick={() => deleteImage(url)}>Remover</button>
+              )}
+            </li>
+          ))}
+        </ul>
+        <Button></Button>
+        <Button onClick={loadNextGaleryPage}>Próximo</Button>
+        <h1>Alunos</h1>
+        {user && (
+          <form onSubmit={(e) => e.preventDefault()}>
+            <label>
+              Nome:{" "}
+              <input
+                type="text"
+                onChange={(e) => setAluno({ ...aluno, name: e.target.value })}
+              />
+            </label>
+            <label>
+              Faixa:{" "}
+              <input
+                type="text"
+                onChange={(e) => setAluno({ ...aluno, belt: e.target.value })}
+              />
+            </label>
+            <label>
+              Foto:{" "}
+              <input
+                type="file"
+                accept="image/*"
+                onChange={(e) => {
+                  setAluno({
+                    ...aluno,
+                    photo: (e.target as HTMLInputElement).files![0],
+                    photoName: (e.target as HTMLInputElement).files![0].name,
+                  });
+                }}
+              />
+            </label>
+            <button onClick={addAluno}>Adicionar</button>
+          </form>
+        )}
 
-      <Alunos />
+        <Alunos />
+      </ThemeProvider>
     </>
   );
 }
